99ffaa9d9a27541ef6d8199c8939d5c4b1df61fe
Daniel Wulff and John Dow <daniel+john@>
2016-06-08 14:09:22 -0700

    created new blog component

---
+++ app/components/new-blog.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  actions: {
+    save() {
+      var params = {
+        title: this.get('title'),
+        author: this.get('author'),
+        content: this.get('content'),
+        storelink: this.get('storelink'),
+        image: this.get('image'),
+      };
+      this.sendAction('save', params);
+    }
+  }
+});
---
+++ app/routes/admin.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+    return Ember.RSVP.hash({
+      blogs: this.store.findAll('blog')
+    });
+  },
+  actions: {
+    save(params) {
+      var newBlog = this.store.createRecord('blog', params);
+      newBlog.save();
+      this.transitionTo('admin');
+    }
+  }
+});
