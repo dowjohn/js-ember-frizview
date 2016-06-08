ac29ac0f5fa254f8c5356dd6959de71488cded4d
Daniel Wulff and John Dow <daniel@>
2016-06-08 10:38:41 -0700

    got blog-tiles to work

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'frizview',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://frizview.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
